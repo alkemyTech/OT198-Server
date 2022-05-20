349d9aea1b1e598ec6a66cf241114e02df56edac
Nicolas Bes <nicobes14@gmail.com>
2022-05-20 01:44:52 -0300

    Refactoring code

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -6,8 +6,9 @@ const { Testimonial } = db
 
 // create testimonial function
 const create = async (req, res, next) => {
+  const { name, image, content } = req.body
   try {
-    const testimonial = await Testimonial.create(req.body)
+    const testimonial = await Testimonial.create({ name, image, content })
     endpointResponse({
       res,
       code: 201,
@@ -15,13 +16,11 @@ const create = async (req, res, next) => {
       message: testimonial,
     })
   } catch (err) {
-    const httpError = createHttpError(
-      err.statusCode,
-      `Error creating testimonial: ${err.message}`,
-    )
+    const httpError = createHttpError(err.statusCode, `Error creating testimonial: ${err.message}`)
     next(httpError)
   }
 }
+
 // find all testimonials function
 const showAll = async (req, res, next) => {
   try {
@@ -40,15 +39,13 @@ const showAll = async (req, res, next) => {
     next(httpError)
   }
 }
+
 // find by id function
 const findById = async (req, res, next) => {
   try {
     const testimonial = await Testimonial.findByPk(req.params.id)
     if (!testimonial) {
-      const httpError = createHttpError(
-        404,
-        'testimonial Not Found',
-      )
+      const httpError = createHttpError(404, 'Testimonial not found')
       next(httpError)
     } else {
       endpointResponse({
@@ -66,43 +63,41 @@ const findById = async (req, res, next) => {
     next(httpError)
   }
 }
+
 // delete testimonial function
 const destroy = async (req, res, next) => {
   try {
     const deletedRows = await Testimonial.destroy({ where: { id: req.params.id } })
-    if (deletedRows === 0) next(createHttpError(404, 'Not Found'))
+    if (deletedRows === 0) next(createHttpError(404, 'Testimonial not found'))
 
     endpointResponse({
       res,
       code: 204,
       status: true,
-      message: 'deleted',
+      message: 'Deleted',
     })
   } catch (err) {
-    const httpError = createHttpError(
-      err.statusCode,
-      `Error deleting testimonial: ${err.message}`,
-    )
+    const httpError = createHttpError(err.statusCode, `Error deleting testimonial: ${err.message}`)
     next(httpError)
   }
 }
+
 // update testimonial function
 const update = async (req, res, next) => {
   try {
     const testimonial = await Testimonial.findByPk(req.params.id)
-    if (!testimonial) next(createHttpError(404, 'Not Found'))
-    await testimonial.update(
-      req.body,
-    )
+    if (!testimonial) next(createHttpError(404, 'Testimonial not found'))
+    const { name, image, content } = req.body
+    await testimonial.update({ name, image, content })
     await testimonial.save()
     endpointResponse({
-      res, code: 200, status: true, message: testimonial,
+      res,
+      code: 200,
+      status: true,
+      message: testimonial,
     })
   } catch (err) {
-    const httpError = createHttpError(
-      err.statusCode,
-      `Error updating testimonial: ${err.message}`,
-    )
+    const httpError = createHttpError(err.statusCode, `Error updating testimonial: ${err.message}`)
     next(httpError)
   }
 }
