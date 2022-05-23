ac264d59e296220e006419c344b366c515e8f3bd
Joaquin Barrionuevo <joaquinbarrionuevo@MacBook-Air-de-Joaquin.local>
2022-05-23 12:53:18 -0300

    create User Model

---
+++ database/models/users.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Users.belongsTo( models.Roles, {
+        foreignKey: 'id',
+        targetKey: 'roleId',
+      })
+    }
+  };
+  Users.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    photo: DataTypes.STRING,
+    roleId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Users',
+    timestamps: true,
+    paranoid: true,
+  });
+  return Users;
+};
