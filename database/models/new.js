e06feeaaaff76e034686f46068430ce82aa5dbb2
Nicolas Bes <nicobes14@gmail.com>
2022-05-25 21:42:47 -0300

    fixes

--- database/models/new.js
+++ database/models/new.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.Category.hasMany(models.New, { foreignKey: 'categoryId' });
       New.belongsTo( models.Category, {
         foreignKey: 'categoryId',
       })
