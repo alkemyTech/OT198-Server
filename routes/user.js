5e367b550cbdde4c8390403b12356379b6b6a6d4
honeybadger2788 <noe.carosella@gmail.com>
2022-05-26 18:57:58 -0300

    fix post user route

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,6 @@ const router = require('express').Router()
 const { post } = require('../controllers/user')
 
 // register new user
-router.post('/', post)
+router.post('/register', post)
 
 module.exports = router
