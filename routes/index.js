3b8602384063c5d090fb7214a507b5605186cf8b
honeybadger2788 <noe.carosella@gmail.com>
2022-05-27 12:57:40 -0300

    replace user router by auth router

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const pingRouter = require('./ping')
 const categoriesRouter = require('./categories')
 const newsRouter = require('./news')
 const organizationRouter = require('./organization')
-const userRouter = require('./user')
+const authRouter = require('./auth')
 
 const router = express.Router()
 
@@ -18,7 +18,7 @@ router.use('/categories', categoriesRouter)
 router.use('/news', newsRouter)
 // organization routes
 router.use('/organization', organizationRouter)
-// user routes
-router.use('/auth', userRouter)
+// auth routes
+router.use('/auth', authRouter)
 
 module.exports = router
