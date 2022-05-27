b9e0327ab52f5dd7186aa04993706974b1b6bb08
Pablo Penalva <penalva.pablo@gmail.com>
2022-05-27 10:18:43 -0300

    validate error middleware added

--- routes/news.js
+++ routes/news.js
@@ -2,12 +2,12 @@ const express = require('express')
 
 const router = new express.Router()
 
-const validateError = require('../middlewares/validateError')
-const newSchemaValidator = require('../schemas/newSchema')
+const { validateSchema } = require('../middlewares/validateErrors')
+const newSchema = require('../schemas/newSchema')
 const { post } = require('../controllers/news')
 const { listNews } = require('../controllers/news')
 
 router.get('/:id', listNews)
-router.post('/', newSchemaValidator, validateError, post)
+router.post('/', validateSchema(newSchema), post)
 
 module.exports = router
