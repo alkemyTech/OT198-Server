05fbc2731f20f4d76ab7e9f7a1e40805faf460f0
Nicolas Bes <nicobes14@gmail.com>
2022-05-27 19:36:17 -0300

    using utils in the service

--- services/categories.js
+++ services/categories.js
@@ -1,31 +1,32 @@
 const db = require('../database/models')
+const { checkUndefined, databaseError, created } = require('../utils')
 
 const { Category } = db
 // todo - create a error handler
 const listCategoryById = async (id) => {
   try {
     const category = await Category.findByPk(id)
-    return category
+    return checkUndefined(category)
   } catch (error) {
-    throw new Error(error)
+    return databaseError(error)
   }
 }
 
 const listCategories = async () => {
   try {
     const categories = await Category.findAll({ attributes: ['name'] })
-    return categories
+    return checkUndefined(categories, 'No categories found')
   } catch (error) {
-    throw new Error(error)
+    return databaseError(error)
   }
 }
 
 const createCategory = async (category) => {
   try {
     const newCategory = await Category.create(category)
-    return newCategory
+    return created(newCategory)
   } catch (error) {
-    throw new Error(error)
+    return databaseError(error)
   }
 }
 
