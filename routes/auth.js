14090c19888be9ffae0cfd7af6fd5aa06c9a29e2
Nicolas Bes <nicobes14@gmail.com>
2022-05-27 15:28:32 -0300

    rename login controller function

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const { userRegisterSchema, userLoginSchema } = require('../schemas/user')
 const { validateSchema } = require('../middlewares/validateErrors')
-const { post } = require('../controllers/user')
+const { post, login } = require('../controllers/user')
 
 // register new user
 router.post('/register', validateSchema(userRegisterSchema), post)
 
 // login user
-router.post('/login', validateSchema(userLoginSchema), post)
+router.post('/login', validateSchema(userLoginSchema), login)
 module.exports = router
