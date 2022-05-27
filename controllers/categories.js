1eaa73eff2cec03cda0b228cac38bb2313ce5bb8
Nicolas Bes <nicobes14@gmail.com>
2022-05-27 19:35:41 -0300

    example how the code has to be in the controller

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,64 +1,40 @@
-const createHttpError = require('http-errors')
 const { endpointResponse } = require('../helpers/success')
 const { listCategories, listCategoryById, createCategory } = require('../services/categories')
 
 const list = async (req, res, next) => {
   try {
-    const categories = await listCategories()
+    const response = await listCategories()
     endpointResponse({
       res,
-      code: 200,
-      status: true,
-      message: 'Categories found',
-      body: categories,
+      ...response,
     })
   } catch (error) {
-    const httpError = createHttpError(
-      error.statusCode,
-      `[Error retrieving categories] - [categories - GET]: ${error.message}`,
-    )
-    next(httpError)
+    next(error)
   }
 }
 
 const listCategory = async (req, res, next) => {
-  const { id } = req.params
   try {
-    const category = await listCategoryById(id)
-    if (!category) throw next(createHttpError(404, `Category with id ${id} not found`))
+    const response = await listCategoryById(req.params.id)
     endpointResponse({
       res,
-      code: 200,
-      status: true,
-      message: 'Category found',
-      body: category,
+      ...response,
     })
   } catch (error) {
-    const httpError = createHttpError(
-      error.statusCode,
-      `[Error with database] - [listCategory with ID - GET]: ${error.message}`,
-    )
-    next(httpError)
+    next(error)
   }
 }
 
 const post = async (req, res, next) => {
   try {
     const { name, description, image } = req.body
-    const category = await createCategory({ name, description, image })
+    const response = await createCategory({ name, description, image })
     endpointResponse({
       res,
-      code: 201,
-      status: true,
-      message: 'Category created',
-      body: category,
+      ...response,
     })
   } catch (error) {
-    const httpError = createHttpError(
-      error.statusCode,
-      `[Error with database] - [create Category - POST]: ${error.message}`,
-    )
-    next(httpError)
+    next(error)
   }
 }
 
