40574e7f46f053c4648bd4d5a5c62845f37cdb0d
Nicolas Bes <nicobes14@gmail.com>
2022-05-31 11:23:44 -0300

    including template to sendgrid service

--- services/sendgrid.js
+++ services/sendgrid.js
@@ -1,14 +1,10 @@
 const sgMail = require('@sendgrid/mail')
+const { templateEmail } = require('../templates/welcomeEmail')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = async (email) => {
   try {
-    const msg = {
-      to: email,
-      from: process.env.SENDGRID_EMAIL,
-      subject: 'Welcome to the app',
-      text: 'Welcome to the app, enjoy!',
-    }
+    const msg = templateEmail(email, process.env.SENDGRID_EMAIL)
     await sgMail.send(msg)
   } catch (error) {
     throw new Error(error)
