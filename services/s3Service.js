48737652dd6944f10ae2ff647db0cb28434936ea
honeybadger2788 <noe.carosella@gmail.com>
2022-05-31 12:32:38 -0300

    create s3 service and client

---
+++ services/s3Service.js
@@ -0,0 +1,19 @@
+const { CreateBucketCommand } = require('@aws-sdk/client-s3')
+const { s3Client } = require('../libs/s3Client')
+
+// Cuando el servicio este conectado, se deberan eliminar los siguientes paquetes
+require('dotenv').config()
+
+// Set the parameters
+const bucketParams = {
+  Bucket: process.env.S3_BUCKET_NAME,
+}
+
+module.exports = async () => {
+  try {
+    const data = await s3Client.send(new CreateBucketCommand(bucketParams))
+    return data
+  } catch (error) {
+    throw new Error(error)
+  }
+}
