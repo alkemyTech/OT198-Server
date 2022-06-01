79dc9468c98f4cbe2b29aff3361e4a2ec61f5694
honeybadger2788 <noe.carosella@gmail.com>
2022-06-01 09:50:20 -0300

    add s3service putObject

--- controllers/ping.js
+++ controllers/ping.js
@@ -1,14 +1,17 @@
 const createHttpError = require('http-errors')
 const { endpointResponse } = require('../helpers/success')
+const { s3Service } = require('../services/s3Service')
 
 module.exports = {
-  pong: (req, res, next) => {
+  pong: async (req, res, next) => {
     try {
+      const data = await s3Service()
       endpointResponse({
         res,
         code: 200,
         status: true,
         message: 'PONG',
+        body: data,
       })
     } catch (error) {
       const httpError = createHttpError(
--- services/s3Service.js
+++ services/s3Service.js
@@ -1,14 +1,24 @@
-const { CreateBucketCommand } = require('@aws-sdk/client-s3')
+const { /* CreateBucketCommand */PutObjectCommand } = require('@aws-sdk/client-s3')
+const fs = require('fs')
+const path = require('path')
 const { s3Client } = require('../libs/s3Client')
 
+const file = path.join(__dirname, '../public/img/LOGO-SOMOS MAS.png')// Path to and name of object. For example '../myFiles/index.js'.
+const fileStream = fs.createReadStream(file)
+
 // Set the parameters
-const bucketParams = {
+const uploadParams = {
   Bucket: process.env.S3_BUCKET_NAME,
+  // Add the required 'Key' parameter using the 'path' module.
+  Key: file,
+  // Add the required 'Body' parameter
+  Body: fileStream,
 }
 
 module.exports = async () => {
   try {
-    const data = await s3Client.send(new CreateBucketCommand(bucketParams))
+    /* const data = await s3Client.send(new CreateBucketCommand(bucketParams)) */
+    const data = await s3Client.send(new PutObjectCommand(uploadParams))
     return data
   } catch (error) {
     throw new Error(error)
