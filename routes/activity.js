c4db49e91911e3a1515c681574d77d0d4ecfc098
honeybadger2788 <noe.carosella@gmail.com>
2022-06-02 10:00:29 -0300

    add middlewares at activity routes

--- routes/activity.js
+++ routes/activity.js
@@ -4,9 +4,11 @@ const router = express.Router()
 
 const { post, update } = require('../controllers/activity')
 const { validateSchema } = require('../middlewares/validateErrors')
+const { isAdmin } = require('../middlewares/isAdmin')
+const { auth } = require('../middlewares/auth')
 const activitySchema = require('../schemas/activity')
 
 router.post('/', validateSchema(activitySchema), post)
-router.put('/:id', validateSchema(activitySchema), update)
+router.put('/:id', auth, isAdmin, validateSchema(activitySchema), update)
 
 module.exports = router
