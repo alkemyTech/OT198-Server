79fe7d781622d0d18656cc5e1542011c7b0fbf6c
Nicolas Bes <nicobes14@gmail.com>
2022-06-06 13:45:01 -0300

    add update function to testimonial service

--- services/testimonial.js
+++ services/testimonial.js
@@ -1,4 +1,12 @@
+const util = require('util')
+const fs = require('fs')
 const { Testimonial } = require('../database/models')
+
+const unlinkFile = util.promisify(fs.unlink)
+
+const ApiError = require('../helpers/ApiError')
+const { uploadImageToS3 } = require('./uploadImageToS3')
+const httpStatus = require('../helpers/httpStatus')
 
 module.exports = {
   createTestimonial: async (testimonialToCreate) => {
@@ -15,5 +23,38 @@ module.exports = {
     } catch (error) {
       throw new Error(error)
     }
+  },
+  /**
+   * Update a testimonial
+   *
+   * @param {object} testimonialToUpdate the testimonial to update in the
+   * database with the new values
+   * @param {request} req the request object from the client to upload the image
+   * @return {object} the testimonial updated or an error
+   */
+  updateTestimonial: async (testimonialToUpdate, req) => {
+    const { id } = testimonialToUpdate
+    const testimonial = await Testimonial.findByPk(id)
+    if (!testimonial) {
+      if (testimonialToUpdate.image) await unlinkFile(req.file.path)
+      throw new ApiError(httpStatus.NOT_FOUND, `Testimonial with id ${id} not found`)
+    }
+    if (!testimonialToUpdate.image) {
+      testimonialToUpdate.image = testimonial.image
+    } else {
+      try {
+        testimonialToUpdate.image = await uploadImageToS3(req)
+      } catch (error) {
+        await testimonial.update(testimonialToUpdate)
+        if (testimonialToUpdate.image) await unlinkFile(req.file.path)
+        throw new ApiError(
+          httpStatus.PARTIAL_CONTENT,
+          'Testimonial content updated but there is a error uploading image',
+        )
+      }
+    }
+
+    const updatedTestimonial = await testimonial.update(testimonialToUpdate)
+    return updatedTestimonial.dataValues
   },
 }
