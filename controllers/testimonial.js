450c34b7077dbf03e97a6a96ef9a5b6d3e7ad39d
Nicolas Bes <nicobes14@gmail.com>
2022-06-06 13:41:53 -0300

    add update function to testimonial controller

--- controllers/testimonial.js
+++ controllers/testimonial.js
@@ -1,6 +1,7 @@
 const createHttpError = require('http-errors')
+const { catchAsync } = require('../helpers/catchAsync')
 const { endpointResponse } = require('../helpers/success')
-const { createTestimonial } = require('../services/testimonial')
+const { createTestimonial, updateTestimonial } = require('../services/testimonial')
 
 module.exports = {
   post: async (req, res, next) => {
@@ -12,4 +13,24 @@ module.exports = {
       return next(createHttpError(500, error))
     }
   },
+  update: catchAsync(async (req, res) => {
+    const { id } = req.params
+    const { name, content, image } = req.body
+    const updatedTestimonial = await updateTestimonial(
+      {
+        id,
+        name,
+        content,
+        image,
+      },
+      req,
+    )
+    return endpointResponse({
+      res,
+      code: 200,
+      status: true,
+      message: 'Testimonial updated successfully',
+      body: updatedTestimonial,
+    })
+  }),
 }
