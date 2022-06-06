4e8f4309b904168a5f6f257081fb7a6dc4e98b4c
Nicolas Bes <nicobes14@gmail.com>
2022-06-06 11:25:26 -0300

    add destroy function to testimonial controller

--- controllers/testimonial.js
+++ controllers/testimonial.js
@@ -1,9 +1,11 @@
 const createHttpError = require('http-errors')
+const { catchAsync } = require('../helpers/catchAsync')
+const httpStatus = require('../helpers/httpStatus')
 const { endpointResponse } = require('../helpers/success')
-const { createTestimonial } = require('../services/testimonial')
+const { createTestimonial, deleteTestimonial } = require('../services/testimonial')
 
 module.exports = {
-  post: async (req, res, next) => {
+  post: catchAsync(async (req, res, next) => {
     try {
       const { name, content, image } = req.body
       const response = await createTestimonial({ name, content, image })
@@ -11,5 +13,15 @@ module.exports = {
     } catch (error) {
       return next(createHttpError(500, error))
     }
-  },
+  }),
+  destroy: catchAsync(async (req, res) => {
+    const { id } = req.params
+    await deleteTestimonial(id)
+    return endpointResponse({
+      res,
+      code: httpStatus.OK,
+      status: true,
+      message: 'Testimonial deleted successfully',
+    })
+  }),
 }
