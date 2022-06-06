106168746b6b5a0248f2f9350d7ec54f59ef5b62
Nicolas Bes <nicobes14@gmail.com>
2022-06-06 13:41:20 -0300

    add route to testimonial router

--- routes/testimonial.js
+++ routes/testimonial.js
@@ -1,10 +1,20 @@
 const router = require('express').Router()
-const { createTestimonialSchema } = require('../schemas/testimonial')
+const { createTestimonialSchema, updateTestimonialSchema } = require('../schemas/testimonial')
 const { validateSchema } = require('../middlewares/validateErrors')
 const { auth } = require('../middlewares/auth')
 const { isAdmin } = require('../middlewares/isAdmin')
-const { post } = require('../controllers/testimonial')
+const { post, update } = require('../controllers/testimonial')
+const { uploadImage } = require('../middlewares/uploadImage')
 
 router.post('/', auth, isAdmin, validateSchema(createTestimonialSchema), post)
+
+router.put(
+  '/:id',
+  auth,
+  isAdmin,
+  uploadImage('image'),
+  validateSchema(updateTestimonialSchema),
+  update,
+)
 
 module.exports = router
