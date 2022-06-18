98531d7aa2850f8916dac9b4640f39e9e4972811
honeybadger2788 <noe.carosella@gmail.com>
2022-06-18 18:55:43 -0300

    add test error cases at post route

--- test/news.test.js
+++ test/news.test.js
@@ -10,6 +10,12 @@ const user = {
     password: 'Test1234'
 }
 
+// User without Admin role
+const notAdminUser = {
+    email: 'mlabern1@army.mil',
+    password: 'Test1234'
+}
+
 const newData = {
     name: 'Test new',
     content: 'Test new content',
@@ -28,6 +34,16 @@ describe('News', () => {
         .end((err, res) => {
             expect(res).to.have.property('status', 200)
             token = res.body.body.token
+            done()
+        })
+    })
+    before((done) => {
+        request(app)
+        .post('/auth/login')
+        .send(notAdminUser)
+        .end((err, res) => {
+            expect(res).to.have.property('status', 200)
+            notAdminToken = res.body.body.token
             done()
         })
     })
@@ -72,6 +88,49 @@ describe('News', () => {
                 expect(res).to.have.property('status', 201)
                 expect(res.body).to.have.property('message', 'New created')
                 idNews = res.body.body.id
+                done()
+            })
+        })
+    })
+    describe('POST /news with errors', () => { 
+        // Test POST /news with validation errors
+        it('should return a 400 error', (done) => {
+            request(app)
+            .post('/news')
+            .set('Authorization', `Bearer ${token}`)
+            .send({ name: '  ', content: '   ' })
+            .end((err, res) => {
+                expect(res).to.have.property('status', 400)
+                expect(res.body.errors).to.be.an('array')
+                done()
+            })
+        })
+        // Test POST /news without headers
+        it('should return a 401 error', (done) => {
+            request(app)
+            .post('/news')
+            .field('name', newData.name)
+            .field('content', newData.content)
+            .field('categoryId', newData.categoryId)
+            .attach('image', path.join(__dirname, newData.image))
+            .end((err, res) => {
+                expect(res).to.have.property('status', 401)
+                expect(res.body).to.have.property('message', 'No authorization header')
+                done()
+            })
+        })
+        // Test POST /news without admin role
+        it('should return a 401 error', (done) => {
+            request(app)
+            .post('/news')
+            .set('Authorization', `Bearer ${notAdminToken}`)
+            .field('name', newData.name)
+            .field('content', newData.content)
+            .field('categoryId', newData.categoryId)
+            .attach('image', path.join(__dirname, newData.image))
+            .end((err, res) => {
+                expect(res).to.have.property('status', 401)
+                expect(res.body).to.have.property('message', 'You are not an admin')
                 done()
             })
         })
